5e76bde86e21c62d26c6a0023a03266b144160e0
Elena <mymoskvina@gmail.com>
2020-10-06 18:53:54 +0300

    Commited cosmetic changes to detect how to test failed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,6 +10,7 @@ class StatisticsServiceTest {
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
+
     long actual = service.findMax(incomesInBillions);
 
     assertEquals(expected, actual);
